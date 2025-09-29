2fe8dc6fef9b24e92d7110b08fc124f381261e59
amissahmary <mamissah2@byupathway.edu>
2025-09-29 02:09:52 +0000

    new file:   week05/Homework/Assignment.cs 	new file:   week05/Homework/MathAssignment.cs 	modified:   week05/Homework/Program.cs 	new file:   week05/Homework/WritingAssignment.cs

---
+++ week05/Homework/Assignment.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Assignment{
+    private string _studentName;
+    private string _topic;
+
+    public Assignment(string name, string topic){
+        _studentName = name;
+        _topic = topic;
+    }
+
+    public string GetSummary(){
+        return $"Student name: {_studentName}\nTopic: {_topic}";
+    }
+
+    public string getName(){
+        return _studentName;
+    }
+}
--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+
+        Assignment assign = new Assignment("Bernice","Calculus");
+        Console.WriteLine(assign.GetSummary());
+
+        Console.WriteLine("\n");
+
+        MathAssignment assigned = new MathAssignment("Frank", "Calculus", "7.3", "1-5");
+        Console.WriteLine(assigned.GetSummary());
+        Console.WriteLine(assigned.GetHomeworkList());
+
+        WritingAssignment assigns = new WritingAssignment("Ochi", "Teenage Pregnancy", "Causes of Teenage Pregnancy");
+        Console.WriteLine(assigns.GetSummary());
+        Console.WriteLine(assigns.GetWritingInformation());
     }
 }
